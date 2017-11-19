519d1d505bb4107142c33cde9b13402265f92140
Anand Raja <Anand@Anands-MacBook-Pro.local>
2017-11-19 12:06:13 -0500

    Add pipeline for IPM

--- ipm.py
+++ ipm.py
@@ -1,14 +1,26 @@
 import numpy as np
 import cv2
 
-# Read in an image with straight lines
-img = cv2.imread('test_images/straight_lines1.jpg')
+def get_ipm_transform():
+    src = np.float32([(536,488),
+                    (751,488),
+                    (241,687),
+                    (1071,687)])
+    dst = np.float32([(450,0),
+                    (1280-450,0),
+                    (450,720),
+                    (1280-450,720)])
+    M    = cv2.getPerspectiveTransform(src, dst)
 
-src = np.float32([(536,488),
-                  (751,488),
-                  (241,687),
-                  (1071,687)])
-dst = np.float32([(450,0),
-                  (1280-450,0),
-                  (450,720),
-                  (1280-450,720)])
+    return M
+
+def inverse_perspective_mapping(img):
+    M = get_ipm_transform()
+    h,w = img.shape[:2]
+    warped = cv2.warpPerspective(img, M, (w,h), flags=cv2.INTER_LINEAR)
+    return warped
+
+img = cv2.imread('test_images/straight_lines1.jpg')
+img_ipm = inverse_perspective_mapping(img)
+
+cv2.imwrite('output_images/ipm_images/straight_lines1.jpg', img_ipm)
