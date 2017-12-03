a9ad5f083b269e1f698191c1bc8651d69771f7aa
Anand <anandraja13@gmail.com>
2017-12-03 10:12:05 -0500

    Try different lane segmentation approach from lecture, add experimental notebook

--- find_lane_lines.py
+++ find_lane_lines.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import matplotlib.pyplot as plt
 
 def window_mask(width, height, img_ref, center, level):
 
@@ -47,12 +48,12 @@ def find_window_centroids(image, window_width, window_height, margin):
         # Find the best right centroid by using past right center as a reference
         r_min_index = int(max(r_center + offset - margin, 0))
         r_max_index = int(min(r_center + offset + margin, image.shape[1]))
-        r_center = argmax(conv_signal[r_min_index:r_max_index])+r_min_index-offset
+        r_center = np.argmax(conv_signal[r_min_index:r_max_index])+r_min_index-offset
 
         # Add found window_centroids
         window_centroids.append((l_center,r_center))
 
-    return find_window_centroids
+    return window_centroids
 
 def plot_window_centroids(warped, window_centroids, window_width, window_height):
 
@@ -75,10 +76,98 @@ def plot_window_centroids(warped, window_centroids, window_width, window_height)
         # Draw the results
         template = np.array(r_points+l_points, np.uint8) # add both left and right window pixels together
         zero_channel = np.zeros_like(template) # create a zero color channel
-        template = np.array(cv2.merge(zero_channel, template, zero_channel), np.uint8) # make window pixels green
+        template = np.array(cv2.merge((zero_channel, template, zero_channel)), np.uint8) # make window pixels green
         warpage = np.dstack((warped, warped, warped))*255 # making the original road pixels 3 color channels
         output = cv2.addWeighted(warpage, 1, template, 0.5, 0.0) # overlay the original road image with window results
 
     # If no window centers found, just display original road image
     else:
         output = np.array(cv2.merge((warped,warped,warped)), np.uint8)
+
+    return output
+
+def find_lane_fit(binary_warped, nwindows=10, margin=40, minpix = 50):
+
+    midy = binary_warped.shape[0]//2
+
+    # Take a histogram of the bottom half of the image
+    histogram = np.sum(binary_warped[midy:,:], axis=0)
+
+    # Create an output image
+    out = np.dstack((binary_warped,binary_warped,binary_warped))*255
+
+    # Find the peak for left and right halves of histogram
+    midx = np.int(histogram.shape[0]/2)
+    leftx_base  = np.argmax(histogram[:midx])
+    rightx_base = np.argmax(histogram[midx:]) + midx
+
+    window_height = np.int(binary_warped.shape[0]/nwindows)
+
+    # Find non-zero pixels
+    nz = binary_warped.nonzero()
+    nzy = np.array(nz[0])
+    nzx = np.array(nz[1])
+
+    leftx_current  = leftx_base
+    rightx_current = rightx_base
+
+    left_idx  = []
+    right_idx = []
+
+    for window in range(nwindows):
+        win_y_low  = binary_warped.shape[0] - (window+1)*window_height
+        win_y_high = binary_warped.shape[0] - (window  )*window_height
+        win_xleft_low   = leftx_current - margin
+        win_xleft_high  = leftx_current + margin
+        win_xright_low  = rightx_current - margin
+        win_xright_high = rightx_current + margin
+
+        good_left_idx = ((nzy >= win_y_low) & (nzy < win_y_high) &
+        (nzx >= win_xleft_low) &  (nzx < win_xleft_high)).nonzero()[0]
+
+        good_right_idx = ((nzy >= win_y_low) & (nzy < win_y_high) &
+        (nzx >= win_xright_low) &  (nzx < win_xright_high)).nonzero()[0]
+
+        left_idx.append(good_left_idx)
+        right_idx.append(good_right_idx)
+
+        # Recenter window if new signal is strong enough
+        if len(good_left_idx) > minpix:
+            leftx_current = np.int(np.mean(nzx[good_left_idx]))
+        if len(good_right_idx) > minpix:
+            rightx_current = np.int(np.mean(nzy[good_right_idx]))
+
+
+    left_idx  = np.concatenate(left_idx)
+    right_idx = np.concatenate(right_idx)
+
+    leftx  = nzx[left_idx]
+    lefty  = nzy[left_idx]
+    rightx = nzx[right_idx]
+    righty = nzy[right_idx]
+
+    # Fit a second order polynomial
+    left_fit  = np.polyfit(lefty, leftx, 2)
+    right_fit = np.polyfit(righty, rightx, 2)
+
+    return left_fit, right_fit, left_idx, right_idx, nzx, nzy
+
+def plot_lane_fit(binary_warped, left_fit, right_fit, left_idx, right_idx, nzx, nzy, nwindows=9, margin=100):
+
+    # Create image for visualization
+    out_img = np.dstack((binary_warped,binary_warped,binary_warped))*255
+
+    # Mark x and y points - left lane in red, right in blue
+    out_img[nzy[left_idx], nzx[left_idx]]   = [255, 0, 0]
+    out_img[nzy[right_idx], nzx[right_idx]] = [0, 0, 255]
+
+    # Plot line fit
+    ploty = np.linspace(0, binary_warped.shape[0]-1,binary_warped.shape[0])
+    left_fitx  = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+    right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+
+    plt.imshow(out_img)
+    plt.plot(left_fitx, ploty, color='yellow')
+    plt.plot(right_fitx, ploty, color='yellow')
+    plt.xlim(0,binary_warped.shape[1])
+    plt.ylim(binary_warped.shape[0], 0)
