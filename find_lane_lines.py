977892657cb654ea22577e86a31281e9e0d4fc86
Anand <anandraja13@gmail.com>
2017-12-03 20:20:58 -0500

    Raw complete pipeline...frame independent

--- find_lane_lines.py
+++ find_lane_lines.py
@@ -150,8 +150,12 @@ def find_lane_fit(binary_warped, nwindows=10, margin=40, minpix=50):
     righty = nzy[right_idx]
 
     # Fit a second order polynomial
-    left_fit  = np.polyfit(lefty, leftx, 2)
-    right_fit = np.polyfit(righty, rightx, 2)
+    left_fit  = None
+    right_fit = None
+    if len(leftx) != 0:
+        left_fit  = np.polyfit(lefty, leftx, 2)
+    if len(rightx) != 0:
+        right_fit = np.polyfit(righty, rightx, 2)
 
     return left_fit, right_fit, left_idx, right_idx, nzx, nzy, rect_corners
 
@@ -251,6 +255,9 @@ def compute_radius_and_center_dist(ipm_img, left_fit, right_fit):
 
     y_eval = ipm_img.shape[0]-1
 
+    if left_fit is None or right_fit is None:
+        return None,None,None
+
     # Fit new polynomials to x,y in world space
     ploty = np.linspace(0, ipm_img.shape[0]-1, ipm_img.shape[0])
     left_fitx  = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
@@ -305,6 +312,10 @@ def draw_nice_lane(img, orig_warped, seg_img, left_fit, right_fit, M_inv):
     return result
 
 def draw_lane_details(orig_img, radius, dist):
+
+    if radius is None or dist is None:
+        return orig_img
+
     img = np.copy(orig_img)
 
     font = cv2.FONT_HERSHEY_DUPLEX
@@ -316,3 +327,27 @@ def draw_lane_details(orig_img, radius, dist):
 
     cv2.putText(img, text, (50, 100), font, 2, (0,255,0), 2, cv2.LINE_AA)
     return img
+
+# Define a class to receive the characteristics of each line detection
+class Line():
+    def __init__(self):
+        # was the line detected in the last iteration?
+        self.detected = False
+        # x values of the last n fits of the line
+        self.recent_xfitted = []
+        #average x values of the fitted line over the last n iterations
+        self.bestx = None
+        #polynomial coefficients averaged over the last n iterations
+        self.best_fit = None
+        #polynomial coefficients for the most recent fit
+        self.current_fit = [np.array([False])]
+        #radius of curvature of the line in some units
+        self.radius_of_curvature = None
+        #distance in meters of vehicle center from the line
+        self.line_base_pos = None
+        #difference in fit coefficients between last and new fits
+        self.diffs = np.array([0,0,0], dtype='float')
+        #x values for detected line pixels
+        self.allx = None
+        #y values for detected line pixels
+        self.ally = None
